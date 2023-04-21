0108ec9061174866a6d8e14f846a9b3b09e3cf38
kabirshuvo <kabirshuvo19@gmail.com>
2023-04-22 00:43:47 +0600

    navbar added

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import { Container } from "react-bootstrap";
 import { Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Store from "./pages/Store";
 
 function App() {
   return (
-    <Container>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/store" element={<Store />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </Container>
+    <>
+      <NavBar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
+    </>
   );
 }
 
