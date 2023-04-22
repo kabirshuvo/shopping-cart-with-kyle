489aa3f7340f19af008198e08dfe497997c86719
kabirshuvo <kabirshuvo19@gmail.com>
2023-04-22 09:51:11 +0600

    LocalStorage Done

---
+++ src/hooks/useLocalStorage.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+
+export function useLocalStorage<T>(key: string, initialValue: T | (() => T)){
+    const [value, setValue] = useState<T>(() => {
+        const jsonValue = localStorage.getItem(key)
+        if (jsonValue != null) return JSON.parse(jsonValue)
+
+        if (typeof initialValue === 'function'){
+            return (initialValue as () => T)()
+        } else {
+            return initialValue
+        }
+    })
+
+    useEffect(()=> {
+        localStorage.setItem(key, JSON.stringify(value))
+    }, [key, value])
+    return [value, setValue] as [typeof value, typeof setValue]
+}
+
+// const useLocalStorage = () => {
+//     return (
+//         <div>
+            
+//         </div>
+//     );
+// };
+
+// export default useLocalStorage;
